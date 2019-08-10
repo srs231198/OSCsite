b3e7eabfd70fdae5dc2b547dc0700ecbc1e1a9de
Shreyas Raj Singh <srs231198@gmail.com>
2019-08-09 22:53:18 -0500

    firstCommit
    Initialized the first page...getting the basic structure in order

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+//make the .nav-item that's clicked active and indicate which page the user is in
+//right now
+$(".nav-item").click(function () {
+    $(".nav-item").removeClass("active");
+    this.addClass("active");
+});
